06f767bee2582d6d8fd2b0fd052c14663fe00bbf
kgytis <kaziukon.gytis@gmail.com>
2022-05-16 08:47:40 +0300

    EJS setup

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ import path from 'path';
 const app = express();
 const port = process.env.PORT;
 
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.resolve('public')));
+
 app.listen(port, () => {
     console.log(`Server is running on PORT http://localhost:${port}`)
 })
